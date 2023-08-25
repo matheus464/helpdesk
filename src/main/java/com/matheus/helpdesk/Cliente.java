a5de6308cdfdcb742e22d28584c08c6fcc899b33
= <mathmonsil@hotmail.com>
2023-08-25 16:43:16 -0300

    anotações classes de dominio

--- src/main/java/com/matheus/helpdesk/Cliente.java
+++ src/main/java/com/matheus/helpdesk/Cliente.java
@@ -1,14 +1,22 @@
 package com.matheus.helpdesk;
 
+import com.matheus.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Cliente extends Pessoa{
+    private static final long serialVersionUID = 1L;
 
+    @OneToMany(mappedBy = "cliente")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Cliente(){
         super();
+        addPerfis(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
--- src/main/java/com/matheus/helpdesk/Tecnico.java
+++ src/main/java/com/matheus/helpdesk/Tecnico.java
@@ -1,14 +1,22 @@
 package com.matheus.helpdesk;
 
+import com.matheus.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Tecnico extends Pessoa{
+    private static final long serialVersionUID = 1L;
 
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico(){
         super();
+        addPerfis(Perfil.TECNICO);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
